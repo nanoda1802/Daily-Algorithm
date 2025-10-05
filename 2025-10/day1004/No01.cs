b78a9b6fdeb8cc6220cfdea45b21e2b335cb3d40
songwongbin <bb201802@naver.com>
2025-10-05 16:08:00 +0900

    [251004] 2문제
    - 문자열 변환 없이 정수 자릿수 다루기 (몇 자리 수인지?, 각 자릿수 합은?)

---
+++ 2025-10/day1004/No01.cs
@@ -0,0 +1,20 @@
+﻿namespace Daily_Algorithm._2025_10.day1004;
+
+// [문제 설명] 자릿수 더하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12931
+
+public class No01
+{
+    public int solution(int n) {
+        int answer = 0;
+        int num = n;
+
+        while (num > 0)
+        {
+            answer += num % 10;
+            num /= 10;
+        }        
+       
+        return answer;
+    }
+}
---
+++ 2025-10/day1004/No02.cs
@@ -0,0 +1,24 @@
+﻿namespace Daily_Algorithm._2025_10.day1004;
+
+// [문제 설명] 자연수 뒤집어 배열로 만들기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12931
+
+public class No02
+{
+    public int[] solution(long n) {
+        int[] answer = new int[(int)Math.Floor(Math.Log10(n) + 1)];
+
+        long num = n;
+       
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = (int) (num % 10);
+            num /= 10;
+        }
+       
+        return answer;
+    }
+}
+
+// [메모] 문자열 사용하지 않고 정수의 길이 알아내는 법
+// https://alpaca-code.tistory.com/21
