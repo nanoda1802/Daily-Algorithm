1ae584097b1cde21c129e8e360b463cf3a79b8ca
songwongbin <bb201802@naver.com>
2025-10-29 22:41:56 +0900

    [251029] 2문제
    - Stack 활용
    - 행렬

---
+++ 2025-10/day1029/No01.cs
@@ -0,0 +1,41 @@
+﻿namespace Daily_Algorithm._2025_10.day1029;
+// 괄호 회전하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/76502
+public class No01
+{  
+    public int solution(string s) {  
+        int answer = 0;  
+        Stack<char> charStack = new Stack<char>();  
+        for (int i = 0; i < s.Length; i++)  
+        {        
+            charStack.Clear();  
+            bool check = true;  
+            for (int j = 0; j < s.Length; j++)  
+            {            
+                char cur = s[(i+j) % s.Length];  
+                switch (cur)  
+                {                
+                    case ')':  
+                        if (!charStack.TryPop(out var c0) || !c0.Equals('(')) check = false;  
+                        break;  
+                    case '}':   
+                        if (!charStack.TryPop(out var c1) || !c1.Equals('{')) check = false;  
+                        break;  
+                    case ']':   
+                        if (!charStack.TryPop(out var c2) || !c2.Equals('[')) check = false;  
+                        break;  
+                    default:   
+                        charStack.Push(cur);  
+                        break;  
+                }        
+            }        
+            if (check && charStack.Count == 0) answer++;  
+        }    
+        return answer;  
+    }
+}
+
+// [메모] 콜스택에 비유해주셔서 이해가 됐음  
+// 메서드가 쌓이다가 블록 마지막에 빼는 것 처럼  
+// 스택에 넣어주다가 닫힌 괄호가 나오면?  
+// pop -> 짝이 맞으면 계속 / 짝이 안 맞으면 break
---
+++ 2025-10/day1029/No02.cs
@@ -0,0 +1,34 @@
+﻿namespace Daily_Algorithm._2025_10.day1029;
+// n^2 배열 자르기
+// https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
+//    [1][2][3][4][5]
+// [1] 1  2  3  4  5 
+// [2] 2  2  3  4  5 
+// [3] 3  3  3  4  5 
+// [4] 4  4  4  4  5 
+// [5] 5  5  5  5  5   
+
+// 0 1 2 3 4  5 6 7 8 9  10 11 12 13 14  15 16 17 18 19  20 21 22 23 24  
+// 1 2 3 4 5  2 2 3 4 5  3  3  3  4  5   4  4  4  4  5   5  5  5  5  5   
+// 행 : (i / n) + 1
+// 열 : (i % n) + 1  
+
+// x행은 x가 x개만큼 있고, 나머지가 (x+1, x+2, ... , n) 으로 채워짐  
+// y열이 x보다 작거나 같으면 해당 칸의 값은 (x)
+// y열이 x보다 크면 해당 칸의 값은 (x+차이 => y)
+public class No02
+{  
+    public int[] solution(int n, long left, long right) {  
+        int[] answer = new int[right-left+1];  
+    
+        for (long i = left; i <= right; i++)  
+        {        
+            int row = (int) (i / n) + 1;  
+            int column = (int) (i % n) + 1;  
+            answer[i - left] = row <= column ? column : row;  
+        }    
+    
+        return answer;  
+    }
+}
