330e0d5452111085647b4fb7b9e5f5cea1a7b4a4
songwongbin <bb201802@naver.com>
2025-11-06 23:29:54 +0900

    [251106] 2문제

---
+++ 2025-11/day1106/No01.cs
@@ -0,0 +1,42 @@
+﻿namespace Daily_Algorithm._2025_11.day1106;
+// 프로세스
+// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+// 프로세스의 중요도가 순서대로 담긴 배열 priorities
+// 정수 1~9, 클수록 우선순위 높음  
+  
+// 몇 번째로 실행되는지 알고싶은 프로세스의 위치를 알려주는 location
+// priorities의 인덱스 값  
+// 해당 프로세스가 몇 번째로 실행되는지 return  
+
+// 1. 실행 대기 큐(Queue)에서 대기중인 프로세스 하나를 꺼냅니다.  
+// 2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣습니다.  
+// 3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행합니다.  
+// 3.1 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료됩니다.  
+public class No01
+{
+    public int solution(int[] priorities, int location) {  
+        int answer = 0;  
+        Queue<(int,int)> process = new Queue<(int,int)>();  
+        for (int i = 0; i < priorities.Length; i++) process.Enqueue((i,priorities[i]));  
+        Array.Sort(priorities,(e1,e2)=>e2-e1);  
+        Queue<int> priority = new Queue<int>(priorities);  
+          
+        while (process.Count > 0)  
+        {        
+            (int curI, int curP) = process.Dequeue();  
+            if (curP == priority.Peek())  
+            {            
+                answer++;            
+                if (curI == location) break;  
+                priority.Dequeue();  
+            }        
+            else  
+            {  
+                process.Enqueue((curI,curP));  
+            }    
+        }    
+        return answer;  
+    }
+}
+
---
+++ 2025-11/day1106/No02.cs
@@ -0,0 +1,76 @@
+﻿namespace Daily_Algorithm._2025_11.day1106;
+// 롤케이크 자르기
+// https://school.programmers.co.kr/learn/courses/30/lessons/132265
+
+// 각 조각에 동일한 가짓수의 토핑이 올라가면 공평하게 롤케이크가 나누어진 것  
+  
+// 토핑들의 번호를 저장한 정수 배열 topping
+// 롤케이크를 공평하게 자르는 방법의 수를 return  
+
+// 토핑들이 [1, 2, 1, 3, 1, 4, 1, 2]
+// 네 번째 토핑(3)과 다섯 번째 토핑(1) 사이를 자르면 [1, 2, 1, 3], [1, 4, 1, 2]
+// 1,2,3 세개, 1,2,4 세개로 공평  
+// [1, 2, 1, 3, 1], [4, 1, 2]으로 잘라도 공평  
+  
+// DP...?  
+// (i = 1) 부터 피벗 출발  
+// 좌측 보관 arr1 + 우측 보관 arr2
+// i가 증가하면?  
+// 우측보관 첫번째 녀석을 좌측보관 마지막에 투입  
+// 공평하면 count++
+// (i = topping.length-1) 까지 반복  
+  
+// 공평한지 판단...?  
+// 딕셔너리로 토핑별 개수 보관...  
+// 토핑 옮길 때 딕셔너리만 최신화  
+// dic.Keys.Count 비교해 같으면 공평...  
+public class No02
+{  
+    public int solution(int[] topping) {  
+        int answer = 0;  
+        Dictionary<int, int> topA = new Dictionary<int, int>();  
+        Dictionary<int, int> topB = new Dictionary<int, int>();  
+    
+        for (int i = 0; i < topping.Length; i++)  
+        {        
+            if (!topB.TryAdd(topping[i], 1)) topB[topping[i]] += 1;  
+        }    
+    
+        for (int i = 1; i < topping.Length; i++)  
+        {        
+            int prev = topping[i - 1];  
+            topB[prev] -= 1;  
+            if (topB[prev] <= 0) topB.Remove(prev);  
+        
+            if (!topA.TryAdd(prev, 1)) topA[prev] += 1;  
+        
+            if (topA.Keys.Count == topB.Keys.Count) answer++;  
+        }    
+    
+        return answer;  
+    }
+}
+
+// [다른풀이] Set 이용 이게 더 깔끔  
+/*
+public int solution(int[] topping) {
+    int answer = 0;
+    HashSet<int> hashsetA = new HashSet<int>();
+    HashSet<int> hashsetB = new HashSet<int>();
+    int[] counts = new int[topping.Length];
+
+    for(int i = 0; i < topping.Length ; i++)
+    {
+        hashsetA.Add(topping[i]);
+        counts[i] = hashsetA.Count;
+    }
+
+    for(int i = topping.Length-1; i > 0 ; i--)
+    {
+        hashsetB.Add(topping[i]);
+        if(counts[i-1] == hashsetB.Count) answer++;
+    }
+
+    return answer;
+}
+*/
