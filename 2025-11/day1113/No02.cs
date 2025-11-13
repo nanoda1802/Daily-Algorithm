3dbdff79e85da63b7e064c545b9fd2c739370ea3
songwongbin <bb201802@naver.com>
2025-11-14 01:13:11 +0900

    [251114] 3문제
    - 비트연산
    - 탐욕법

---
+++ 2025-11/day1113/No02.cs
@@ -0,0 +1,61 @@
+﻿namespace Daily_Algorithm._2025_11.day1113;
+// 두 개 이하로 다른 비트
+// https://school.programmers.co.kr/learn/courses/30/lessons/77885
+
+// f(x) = x보다 크고 x와 비트가 1~2개 다른 수들 중에서 제일 작은 수  
+// numbers 모든 수들에 대하여 각 수의 f 값을 배열에 차례대로 담아 return
+// 0 ≤ numbers의 모든 수 ≤ 10^15  
+
+// 해설 https://school.programmers.co.kr/questions/74664  
+// [풀이1] if ((num & bin) == 0)  
+// &은 두 자리가 모두 1인 비트만 1을 반환  
+// bin은 2의 지수기 때문에 1, 10, 100, 1000, ... 처럼 단 하나의 비트만 1을 가짐  
+// 즉, bin과 비교한다면 특정 비트만 확인이 가능하고,  
+// num의 특정 비트가 1이라면 &연산 결과는 1, 10, 100, 등 bin과 같은 값이 나오겠지만,  
+// 0이라면 무조건 0인 결과가 나옴 => num의 비트 중 가장 처음의 0을 찾을 수 있게 됨!  
+  
+// [풀이2] num |= bin;  
+// 찾은 0을 1로 바꿔줘야 함!  
+// bin은 특정 비트 제외하곤 모두 0이기 때문에,  
+// OR 연산 시 해당 자리 제외하곤 num의 비트에 영향을 미치지 않음  
+// bin은 딱 해당 위치만 1이기 때문에, 저 연산의 결과는 num에서 찾은 0만 1로 변한 값이 됨  
+  
+// [풀이3] num ^= (bin / 2);  
+// 처음 찾은 0위치의 바로 전 위치는 1이겠지?  
+// 여길 0으로 바꿔야하기 때문에, XOR 연산을 활용  
+// XOR 연산은 두 비트가 같다면 0, 다르면 1을 반환하기 때문에,  
+// 바로 전 위치의 비트만 1인 수를 num과 연산해줘야 함!  
+// 그 수가 바로 0을 찾은 bin에 2를 나눈 수!  
+// 예를 들어 bin이 8 (1000) 이었다면 네번째 비트가 0이니까,  
+// 세번째 비트를 바꾸기 위해 100이 필요하고, 2를 나눈 4가 바로 100! 
+ 
+// [풀이4] bin *= 2;  
+// num의 각 비트를 비교하기 위해 자리수를 이동  
+// bin이 1로 시작해 2, 4, 8, 16, ... 으로 늘어가는 것  
+// => 1로 시작해 10, 100, 1000, 10000, ... 으로 비트가 변해가는 것  
+
+public class No02
+{
+    public long[] solution(long[] numbers) {  
+        long[] answer = new long[numbers.Length];  
+  
+        for (int i = 0; i < numbers.Length; i++)  
+        {        
+            long num = numbers[i];  
+            long bin = 1;  
+            while (true)  
+            {            
+                if ((num & bin) == 0)  
+                {                
+                    num |= bin;               
+                    num ^= (bin / 2);  
+                    break;  
+                }           
+                bin *= 2;  
+            }                
+            answer[i] = num;  
+        }    
+        return answer;  
+    }
+}
+
--- Test.cs
+++ Test.cs
@@ -1,4 +1,4 @@
-﻿using Daily_Algorithm._2025_11.day1111;
+﻿using Daily_Algorithm._2025_11.day1113;
 
 namespace Daily_Algorithm;
 
@@ -6,10 +6,8 @@ public class Test
 {
     public static void Main()
     {
-        var a= new No02();
-        Console.WriteLine(a.solution([4,3,1,2,5]));
-        Console.WriteLine(a.solution([2, 1, 4, 3, 6, 5, 8, 7, 10, 9]));
-        Console.WriteLine(a.solution([2, 1, 6, 7, 5, 8, 4, 9, 3, 10]));
+        var a= new No01();
+        Console.WriteLine(a.solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]));
     }
 }
 
