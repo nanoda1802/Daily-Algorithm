49ebf8f1845dded26a63321ecf6da0d59fc40279
songwongbin <bb201802@naver.com>
2025-11-14 01:13:11 +0900

    [251113] 3문제
    - 비트연산
    - 탐욕법

---
+++ 2025-11/day1113/No01.cs
@@ -0,0 +1,56 @@
+﻿namespace Daily_Algorithm._2025_11.day1113;
+// 주차 요금 계산
+// https://school.programmers.co.kr/learn/courses/30/lessons/92341
+
+// fees [180, 5000, 10, 600]
+// 기본시간, 기본요금, 단위시간, 단위요금
+// 시간은 분 단위
+// 누적시간 <= 기본시간 : 기본요금
+// 누적시간 > 기본시간 : 기본요금 + 초과분 단위요금
+// 누적시간 : 하루 총 이용 시간 
+
+// records ["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]
+// "시각 차번 내역" 공백으로 구분
+// 시각 기준 오름차순
+// 입차는 했는데 출차가 없으면 23:59에 출차한 걸로 간주
+
+// 차량 번호가 작은 자동차부터 청구할 주차 요금을 차례대로 정수 배열에 담아서 return
+
+public class No01
+{
+    public int[] solution(int[] fees, string[] records) {
+        // in이면 -time, out이면 +time 하면 누적시간 나옴
+        // 마지막 out이 없으면 무조건 0이하정수? -> 23:59 더해주기
+
+        SortedList<string, int> dic = new SortedList<string, int>();
+        
+        foreach (string record in records)
+        {
+            string[] ctx = record.Split(' ');
+            dic.TryAdd(ctx[1], 0);
+            int time = ToMinute(ctx[0]);
+            dic[ctx[1]] += ctx[2].StartsWith('I') ? -time : time;
+        }
+
+        int[] answer = new int[dic.Count];
+        int idx = 0;
+        
+        foreach (KeyValuePair<string, int> pair in dic) // 순회중 콜렉션 요소의 값을 수정하지 않도록 하자...ㅠ
+        {
+            int totalTime = pair.Value <= 0 ? pair.Value + ToMinute("23:59") : pair.Value;
+            int extraTime = totalTime - fees[0];
+            int fee = fees[1];
+            if (extraTime > 0) fee += (extraTime + fees[2] - 1) / fees[2] * fees[3];
+            answer[idx++] = fee;
+        } 
+        
+        return answer;
+    }
+
+    private int ToMinute(string time)
+    {
+        string[] hm = time.Split(':');
+        return (int.Parse(hm[0]) * 60) + int.Parse(hm[1]);
+    }
+}
+
---
+++ 2025-11/day1113/No03.cs
@@ -0,0 +1,180 @@
+﻿namespace Daily_Algorithm._2025_11.day1113;
+// 두 큐 합 같게 만들기
+// https://school.programmers.co.kr/learn/courses/30/lessons/118667
+
+// [이 문제의 핵심]
+// 1. 두 큐에서 원소를 옮기는 건 순환 형태 -> 아무리 옮겨도 전체 순서는 유지가 됨!
+//   무한정 옮기다 보면 초기 상태로 회귀하기 때문에, "적정 반복 횟수"가 존재
+// 2. 어디서 넣을지 뺄지 -> 탐욕법 활용
+//    "당장" 목표보다 작다? 그럼 받아오고, 크다? 그럼 뽑아주고 
+
+public class No03
+{
+    public int solution(int[] queue1, int[] queue2) {
+        int len = queue1.Length;
+        Queue<int> q1 = new Queue<int>();
+        Queue<int> q2 = new Queue<int>();
+        long sum1 = 0; 
+        long sum2 = 0;
+        for (int i = 0; i < len; i++)
+        {
+            sum1 += queue1[i];
+            sum2 += queue2[i];
+            q1.Enqueue(queue1[i]);
+            q2.Enqueue(queue2[i]);
+        }
+
+        if ((sum1 + sum2) % 2 != 0) return -1;
+
+        long target = (sum1 + sum2) / 2;
+        int cnt = 0;
+        while (cnt < len*3)
+        {
+            if (sum1 < target)
+            {
+                int e = q2.Dequeue();
+                sum1 += e;
+                q1.Enqueue(e);
+            }
+            else if (sum1 > target)
+            {
+                int e = q1.Dequeue();
+                sum1 -= e;
+                q2.Enqueue(e);
+            }
+            else return cnt;
+            cnt++;
+        }
+        
+        return -1;
+    }
+    
+    public int solution2(int[] queue1, int[] queue2) {
+        int len = queue1.Length;
+        Queue<int> q1 = new Queue<int>();
+        Queue<int> q2 = new Queue<int>();
+        long sum1 = 0; 
+        long sum2 = 0;
+        for (int i = 0; i < len; i++)
+        {
+            sum1 += queue1[i];
+            sum2 += queue2[i];
+            q1.Enqueue(queue1[i]);
+            q2.Enqueue(queue2[i]);
+        }
+
+        if ((sum1 + sum2) % 2 != 0) return -1;
+
+        long target = (sum1 + sum2) / 2;
+        int cnt = 1; // 여기가 달랐어......... 그래서 이미 같은 경우에 1을 반환해서 2번과 29번을 실패했던 것
+        while (cnt < len*3)
+        {
+            if (sum1 < target)
+            {
+                int e = q2.Dequeue();
+                sum1 += e;
+                q1.Enqueue(e);
+            }
+            else if (sum1 > target)
+            {
+                int e = q1.Dequeue();
+                sum1 -= e;
+                q2.Enqueue(e); 
+            }
+
+            if (sum1 == target) return cnt;
+            cnt++;
+        }
+        
+        return -1;
+    }
+}
+
+        
+// [실패! 오답 + 시간초과]
+// half가 되려면 a개 주고, b개 받아야 하는지 -> a+b 가 답
+// 235512 - 664132 (18-22) (반:20)
+// 2 5 "10" 15 16 18 (a=3)
+// 6 "12" 16 17 20 22 (b=2)
+// 51266 - 4132235 (20-20)
+/*
+int len = queue1.Length;
+        
+long sum1 = 0;
+foreach (int i in queue1) sum1 += i;
+long total = sum1;
+foreach (int i in queue2) total += i;
+
+if (total % 2 != 0) return -1;
+        
+for (int i1 = 0; i1 < len; i1++)
+{
+    long temp = sum1 -= queue1[i1];
+    int i2 = 0;
+    while (temp < total/2) temp += queue2[i2++];
+    if (temp == total/2) return (i1+1)+i2;
+}
+        
+return -1;
+*/
+/*
+int len = queue1.Length;
+        
+long[] prefixSum1 = new long[len];
+prefixSum1[0] = queue1[0];
+long[] prefixSum2 = new long[len];
+prefixSum2[0] = queue2[0];
+        
+for (int i = 1; i < len; i++)
+{
+    prefixSum1[i] = queue1[i] + prefixSum1[i - 1];
+    prefixSum2[i] = queue2[i] + prefixSum2[i - 1];
+}
+        
+if ((prefixSum1[len-1] + prefixSum2[len-1]) % 2 != 0) return -1;
+        
+long target = (prefixSum1[len-1] + prefixSum2[len-1]) / 2 - prefixSum1[len-1];
+
+for (int i = 0; i < len; i++)
+{
+    for (int j = 0; j < len; j++)
+    {
+        if (target == prefixSum2[j]-prefixSum1[i])
+        {
+            return i + j + 2;
+        }
+    }
+}
+
+return -1;
+*/
+
+// [실패 오답 + 시간초과]
+// 슬라이딩윈도우?
+// 윈도우 크기 늘려가며...
+// 윈도우 크기가 len부터 1씩 감소 (len을 넘으면 이미 확인한 케이스)
+// 윈도우가 이동하며 합 확인 => 합이 total의 절반일 때 반환
+/*
+int len = queue1.Length;
+long total = 0;
+int[] arr = new int[len * 2];
+for (int i = 0; i < len; i++) total += arr[i] = queue1[i];
+for (int i = 0; i < len; i++) total += arr[i + len] = queue2[i];
+
+if (total % 2 != 0) return -1;
+        
+for (int size = len; size >= 1; size--)
+{
+    long sum = 0;
+    for (int i = 0; i < size; i++) sum += arr[i];
+    if (sum == total / 2) return 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        sum += arr[(i+size)%len] - arr[i];
+        if (sum == total / 2) return 0;
+    }
+}
+
+return -1;
+*/
