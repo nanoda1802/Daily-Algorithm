5316aed4520e85fab8529d6ff4138208702d9e4b
songwongbin <bb201802@naver.com>
2025-11-28 22:49:33 +0900

    [251128] 2문제

---
+++ 2025-11/day1128/No01.cs
@@ -0,0 +1,55 @@
+﻿namespace Daily_Algorithm._2025_11.day1128;
+// 무인도 여행
+// https://school.programmers.co.kr/learn/courses/30/lessons/154540
+
+// 섬을 하나 만나면, 그 섬을 "다" 돌아봐야함 dfs?  
+
+// 필드를 활용해갖고 클래스 통째로 들구옴
+// 드디어 dfs를 해냈어!!! 핵심은 반복문 내부에서의 재귀 발동과 return값의 누적이여야요!
+public class No01
+{
+    bool[,] visited;  
+    int[] dy = { 1, -1, 0, 0 };  
+    int[] dx = { 0, 0, 1, -1 };  
+    // maps를 순회하다 X가 아니고, 방문하지 않은 칸을 만나면 섬 도착  
+    // 섬의 상하좌우를 돌며 이 섬의 체류 가능일 합산 + 해당 좌표엔 방문 표시  
+    public int[] solution(string[] maps) {  
+        int lenX = maps[0].Length;  
+        int lenY = maps.Length;  
+        visited = new bool[lenY, lenX];  
+        List<int> islandInfo = new List<int>();  
+        for (int y = 0; y < lenY; y++)  
+        {            
+            for (int x = 0; x < lenX; x++)  
+            {                
+                if (visited[y, x]) continue;  
+                visited[y, x] = true;  
+                if (maps[y][x] == 'X') continue;  
+                int sum = DFS(maps, x, y, lenX, lenY);  
+                islandInfo.Add(sum);  
+            }        
+        }  
+        if (islandInfo.Count <= 0) return new int[] {-1};  
+        islandInfo.Sort();  
+        return islandInfo.ToArray();  
+    }  
+    // 중단 조건? 더 이상 갈 수 있는 칸이 없을 때  
+    // 갈 수 있는 칸? 상하좌우로 연결돼있으며, 방문한 적 없는 칸  
+    private int DFS(string[] maps, int x, int y, int lenX, int lenY)  
+    {        
+        int sum = maps[y][x] - '0';  
+  
+        for (int i = 0; i < 4; i++)  
+        {            
+            int nextX = x + dx[i], nextY = y + dy[i];  
+            if (nextX < 0 || nextY < 0) continue;  
+            if (nextX >= lenX || nextY >= lenY) continue;  
+            if (visited[nextY, nextX]) continue;  
+            if (maps[nextY][nextX] == 'X') continue;  
+            visited[nextY, nextX] = true;  
+            sum += DFS(maps, nextX, nextY, lenX, lenY);  
+        }        
+        return sum;  
+    }
+}
+
---
+++ 2025-11/day1128/No02.cs
@@ -0,0 +1,66 @@
+﻿namespace Daily_Algorithm._2025_11.day1128;
+// 행렬 테두리 회전하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/77485
+
+// 매 회전의 숫자 중 최소값을 순서대로 담은 배열 return
+// x1,y1 에서부터 오른쪽으로 x2-x1칸, 아래로 y2-y1칸, 왼쪽으로 x2-x1칸, 위로 y2-y1칸  
+
+// 내 공간지각력에 짜증이 ㄴ ㅏ... 
+// 이차원 배열을 좌표평면처럼 쓸 거면 가로가 y고 세로가 x
+public class No02
+{
+    private int[,] table;  
+    private int[] dy = { 0, 1, 0, -1 };  
+    private int[] dx = { 1, 0, -1, 0 };  
+    private int[] moveLimit = new int[4]; 
+     
+    public int[] solution(int rows, int columns, int[,] queries)  
+    {        
+        int num = 1;  
+        table = new int[rows, columns];  
+        for (int i = 0; i < rows; i++)  
+        {            
+            for (int j = 0; j < columns; j++)  
+            {                
+                table[i, j] = num++;  
+            }        
+        }  
+        
+        int rotateCount = 0;  
+        int[] answer = new int[queries.GetLength(0)];  
+        for (int i = 0; i < queries.GetLength(0); i++)  
+        {            
+            moveLimit[0] = moveLimit[2] = queries[i, 3] - queries[i, 1];  
+            moveLimit[1] = moveLimit[3] = queries[i, 2] - queries[i, 0];  
+            answer[rotateCount++] = RotateAndFindMinValue(queries[i,1]-1,queries[i,0]-1);  
+        }  
+        
+        return answer;  
+    }    
+    
+    private int RotateAndFindMinValue(int x, int y)  
+    {        
+        int min = int.MaxValue;  
+        int prev = table[y,x], cur = 0;  
+        int moveCount = 1;  
+        
+        for (int i = 0; i < 4; i++)  
+        {            
+            while (moveCount <= moveLimit[i])  
+            {                
+                x += dx[i]; y += dy[i];  
+	            
+                cur = table[y,x];  
+                table[y,x] = prev;  
+                prev = cur;  
+                
+                moveCount++;                
+                if (min > cur) min = cur;  
+            }            
+            moveCount = 1;   
+        } 
+        
+        return min;  
+    }
+}
+
